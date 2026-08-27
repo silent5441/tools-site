b02bf9901cc1b156578c0aa0aba3f205bc4b0ad4
silent5441 <silent5441@users.noreply.github.com>
2026-08-27 16:42:33 +0000

    Fix detached ArrayBuffer: clone buffer before each pdf.js/pdf-lib use

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='freeonlinetools-v3';
+const CACHE_NAME='freeonlinetools-v4';
 const ASSETS=['/','/index.html','/css/style.css','/manifest.json'];
 
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
