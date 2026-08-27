1a55edfa0ee14753ac63c973da64f89065d32e2f
silent5441 <aryanverma869957@gmail.com>
2026-08-27 04:11:45 +0000

    feat: switch to ilovetools.pro custom domain, update all URLs, sitemap, PWA manifest, service worker

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME='freeonlinetools-v1';
-const ASSETS=['/tools-site/','/tools-site/index.html','/tools-site/css/style.css','/tools-site/manifest.json'];
+const CACHE_NAME='freeonlinetools-v2';
+const ASSETS=['/','/index.html','/css/style.css','/manifest.json'];
 
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{if(resp.status===200&&resp.type==='basic'){const clone=resp.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,clone))}return resp}).catch(()=>caches.match('/tools-site/index.html'))))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{if(resp.status===200&&resp.type==='basic'){const clone=resp.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,clone))}return resp}).catch(()=>caches.match('/index.html'))))});
