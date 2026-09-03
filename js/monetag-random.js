638c6c143954192aff12a213985baaa55ca1b93c
silent5441 <54920750+silent5441@users.noreply.github.com>
2026-09-03 00:54:32 +0000

    Add randomized Monetag ad (25% chance) to all tool pages via shared monetag-random.js

---
+++ js/monetag-random.js
@@ -0,0 +1,10 @@
+(function(){
+  if(Math.random() < 0.25){
+    try{
+      var s=document.createElement('script');
+      s.dataset.zone='11712795';
+      s.src='https://al5sm.com/tag.min.js';
+      (document.documentElement||document.body).appendChild(s);
+    }catch(e){}
+  }
+})();
